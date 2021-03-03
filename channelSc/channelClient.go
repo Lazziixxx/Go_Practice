161d512267997a402561edb036ce1be20652ebb8
ZhangWentao <Lazziixxx@gmail.com>
2021-03-03 14:50:43 +0800

    Update channelClient.go

--- channelSc/channelClient.go
+++ channelSc/channelClient.go
@@ -30,3 +30,9 @@ func mustCopy(dst io.Writer,  src io.Reader) {
 		}
 }
 
+/*
+TEST1:
+当输入end时，服务侧关闭连接 io.Copy(os.Stdout, conn) 客户端结束读取 打印done 但是主goroutine无法退出 因为stdin没有收到结束符
+当输入ctrl+z时 主goroutine关闭对conn的写操作 阻塞等待Channel done获取信号 
+同时 服务侧对conn的读操作被关闭 不再scan内容并关闭服务侧的写操作，此时，客户端感知对conn的读操作关闭，打印done并信号到channel，主goroutine读到信号，结束程序
+*/
