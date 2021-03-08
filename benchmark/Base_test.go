e6b5e0d7d7d1c63908bd21a52f8fdb1d78a32b38
ZhangWentao <Lazziixxx@gmail.com>
2021-03-08 09:43:11 +0800

    Create Base_test.go

---
+++ benchmark/Base_test.go
@@ -0,0 +1,16 @@
+package main
+
+import "testing"
+
+func fib(n int) int {
+  if n == 0 || n == 1 {
+    return n
+  }
+  return fib(n-2) + fib(n-1)
+}
+
+func BenchmarkFib(b *testing.B) {
+  for n:=0; n < b.N; n++ {
+    fib(30)
+  }
+}
