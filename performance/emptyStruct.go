e050bec0c6fe3936af0ad4be459c6c428a5ec4ac
ZhangWentao <Lazziixxx@gmail.com>
2021-03-10 09:26:59 +0800

    Update emptyStruct.go

--- performance/emptyStruct.go
+++ performance/emptyStruct.go
@@ -22,12 +22,31 @@ func (s Set) Delete(key string) {
 
 /*********************************************/
 /* 用法2：不发送数据的信道 Channel */
+/* channel不发送任何数据  只用来通知子协程执行任务 */
+func worker(ch chan struct{}) {
+  <- ch
+  fmt.Println("do something")
+  close(ch)
+}
 
+func main() {
+  ch := make(chan struct{})
+  go worker(ch)
+  ch <- struct{}{}
+}
 /*********************************************/
 
 
 /*********************************************/
 /* 用法3：仅包含方法的结构体 */
+type door struct{}
 
+func (d door) Open() {
+  ....
+}
+
+func (d door) Close() {
+  ....
+}
 /*********************************************/
 
